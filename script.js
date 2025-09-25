cf021852ab6af10f47476f4fa95705fb51bd6dfd
Meshkat <alimuzzamanmeskat@gmail.com>
2025-09-26 05:42:44 +0600

    matha ta hang

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
-document.getElementById("valobasha").addEventListener("click", function(){
-     console.log( "atleast working")
-})
+// document.getElementById("valobasha").addEventListener("click", function(){
+//      console.log( "atleast working")
+// })
    
+ 
+    let valobashaNum = 0;
+    let shonaNum = 100; 
+
+    const valobashaDisplay = document.getElementById("valobasha"); 
+    const shonaDisplay = document.getElementById("shona"); 
+
+    const turuLove = document.getElementsByClassName("turu-love")
+    
+ for (let i = 0; i < turuLove.length; i++) {
+    turuLove[i].addEventListener("click", function () {
+        valobashaNum++;
+        valobashaDisplay.innerText = valobashaNum;
+    });
+}
